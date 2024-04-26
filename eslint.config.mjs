c836f940fb387f84e57532b0149540d7b3c861bd
Chris Pilkington <chris.pilkington@gmail.com>
2024-04-26 13:24:55 +1000

    Use eslint-plugin-jsonc plugin to parse json files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 // https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
 
 import js from "@eslint/js";
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
 
 export default [
   js.configs.recommended,
+  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
   {
-    // your configuration here
+    rules: {
+      // override/add rules settings here, such as:
+      // 'jsonc/rule-name': 'error'
+    }
   }
 ];
