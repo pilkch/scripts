79850b9ea7182a89044121af8cb0cea0ccffb0e3
Chris Pilkington <chris.pilkington@gmail.com>
2024-04-26 10:35:23 +1000

    Run eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
+
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    // your configuration here
+  }
+];
